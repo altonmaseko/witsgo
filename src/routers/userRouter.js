45dc22e1ff0895e179caa24a93c35bf5d3492d99
Alton Maseko <altonmaseko1000@gmail.com>
2024-09-16 23:13:13 +0200

    updating

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const passport = require("passport");
 const { updateUserController, getUserController } = require("../controllers/userControllers");
 
+require("dotenv").config();
+
 // create route for updating user details
 router.put("/user/update/:email", updateUserController);
 router.get("/user/:email", getUserController);
+router.get("/api/secrets/googlemapsapikey",
+    // return google maps api key
+    (req, res) => {
+        console.log("google maps api key requested");
+        res.json({
+            googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+        });
+    }
+);
 
 module.exports = router;
 
