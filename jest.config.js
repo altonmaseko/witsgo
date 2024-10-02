61fe6b9152317742c897b97c4616241ab7bc5854
lucianIrsigler <l.irsigler64@gmail.com>
2024-10-02 20:40:15 +0200

    updated workflow

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    testMatch: ["**/tests/**/*.test.js"]
+    testMatch: ["**/tests/**/*.test.js"],
+    reporters: [
+        "default", // Default Jest reporter
+        [
+            "jest-junit",
+            {
+                outputDirectory: "./reports/jest", // Directory to output JUnit reports
+                outputName: "junit.xml", // Name of the output file
+            },
+        ],
+    ],
   };
